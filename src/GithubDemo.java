8eb85517eb6ffe853e4955f4d1c5ab2b5c0cc93d
wfd <715294017@qq.com>
2020-01-30 10:12:15 +0800

    创建项目和创建了一个文件

---
+++ src/GithubDemo.java
@@ -0,0 +1,10 @@
+/**
+ * @description:
+ * @author: wfd
+ * @time: 2020/1/30 10:11
+ */
+public class GithubDemo {
+    public static void main(String[] args) {
+        System.out.println("学习github管理项目");
+    }
+}
