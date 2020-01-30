187c3fbe5b56d1fd98787769581fcdbe66ca54b8
wfd <715294017@qq.com>
2020-01-30 10:14:55 +0800

    添加了一个文件

---
+++ src/GithubDemo2.java
@@ -0,0 +1,10 @@
+/**
+ * @description:
+ * @author: wfd
+ * @time: 2020/1/30 10:14
+ */
+public class GithubDemo2 {
+    public static void main(String[] args) {
+        System.out.println("创建了第二个文件");
+    }
+}
